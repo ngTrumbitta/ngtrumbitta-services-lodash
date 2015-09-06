d8d3e70b767b98620e20d1fd63bb54cbfe598200
William Ghelfi <trumbitta@gmail.com>
2015-09-06 19:49:08 +0200

    Delete unused files

--- lodash.service.js
+++
@@ -1,9 +0,0 @@
-var lodashService = angular.module('ngtrumbitta.services.lodash', []);
-
-lodashService.factory('_', ['$window',
-    function($window) {
-      'use strict';
-      return $window._;
-    }
-  ]
-);
